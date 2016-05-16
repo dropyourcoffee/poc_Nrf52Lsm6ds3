b6913d94f32a12adc6b5e8ba0286aff689efec02
Dexter <dexter@zikto.com>
2016-05-16 10:26:30 +0900

    init

---
+++ pca10036/s132/arm5_no_packs/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'nRF_projbase' 
+ * Target:  'nrf52_s132_PCA10040' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
